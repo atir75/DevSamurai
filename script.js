5d4e6c2e983778e383406d067b81a81b759b31cf
Maria Rita Mendonça <mariamendonca.gbmtech@outlook.com.br>
2023-01-03 21:12:09 -0300

    commit initial

---
+++ script.js
@@ -0,0 +1,8 @@
+//desafio 1 
+//Incremente a nossa calculara para receber os valores a partir dos dados digitados pelo usuário e mostrar
+//como um alerta.
+
+var x = prompt("Digite o primeiro número");
+var y = prompt("Digite o segundo número");
+var resultado = Number(x) + Number(y);
+alert("O resultado da soma é: " + resultado);
