6a0c9edc778d64d8a3607aa26ba0f67bd910bff3
Maria Rita Mendonça <mariamendonca.gbmtech@outlook.com.br>
2023-01-05 20:06:14 -0300

    commit desafio 2

--- script.js
+++ script.js
@@ -2,8 +2,34 @@
 //Incremente a nossa calculara para receber os valores a partir dos dados digitados pelo usuário e mostrar
 //como um alertaa.
 
+ var x = prompt("Digite o primeiro número");
+ var a = prompt("Digite o segundo numero: ");
+ var y = prompt("Digite o terceiro número");
+ var resultado = Number(x) + Number(y);
+
+ alert("O resultado da soma é: " + resultado);
+
+
+// var idade = 20;
+// var genero = "F";
+
+// if ((idade <= 18) && (genero == "F")){
+//     console.log("Opa");
+// }else {
+//     console.log("ai sim")
+// }
+
+
+
+// desafio 2 
+// No nosso desafio da calculadora, criar um IF para verificar se o valor é maior que 10, caso seja, informar
+// uma mensagem escolhida por você.
+
 var x = prompt("Digite o primeiro número");
-var a = prompt("Digite o segundo numero: ")
+var a = prompt("Digite o segundo numero: ");
 var y = prompt("Digite o terceiro número");
 var resultado = Number(x) + Number(y);
-alert("O resultado da soma é: " + resultado);
+if (resultado > 10) {
+    alert("o resultado é maior que 10!")
+ }
+ alert("O resultado da soma é: " + resultado);
