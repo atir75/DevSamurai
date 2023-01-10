52bcae70d4b4990af4bcd55c058344bc1a7eed56
Maria Rita Mendonça <maria.rita27012@gmail.com>
2023-01-10 20:31:18 -0300

    commit js avancado aula 1(funcoes)

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 //Incremente a nossa calculara para receber os valores a partir dos dados digitados pelo usuário e mostrar
 //como um alertaa.
 
- var x = prompt("Digite o primeiro número");
- var a = prompt("Digite o segundo numero: ");
- var y = prompt("Digite o terceiro número");
- var resultado = Number(x) + Number(y);
+//  var x = prompt("Digite o primeiro número");
+//  var a = prompt("Digite o segundo numero: ");
+//  var y = prompt("Digite o terceiro número");
+//  var resultado = Number(x) + Number(y);
 
- alert("O resultado da soma é: " + resultado);
+//  alert("O resultado da soma é: " + resultado);
 //
 
 
@@ -15,14 +15,14 @@
 // No nosso desafio da calculadora, criar um IF para verificar se o valor é maior que 10, caso seja, informar
 // uma mensagem escolhida por você.
 
-var x = prompt("Digite o primeiro número");
-var a = prompt("Digite o segundo numero: ");
-var y = prompt("Digite o terceiro número");
-var resultado = Number(x) + Number(y);
-if (resultado > 10) {
-    alert("o resultado é maior que 10!")
- }
- alert("O resultado da soma é: " + resultado);
+// var x = prompt("Digite o primeiro número");
+// var a = prompt("Digite o segundo numero: ");
+// var y = prompt("Digite o terceiro número");
+// var resultado = Number(x) + Number(y);
+// if (resultado > 10) {
+//     alert("o resultado é maior que 10!")
+//  }
+//  alert("O resultado da soma é: " + resultado);
 
 
  // var idade = 20;
@@ -33,3 +33,20 @@ if (resultado > 10) {
 // }else {
 //     console.log("ai sim")
 // }
+
+
+
+//AULA FUNÇÕES
+// function sayHello(name) {
+//     console.log(`salve ${name}`)
+// }
+
+// sayHello("Maria")
+
+
+
+//AULA RETORNO
+// function sum(n1, n2){
+//     return result = n1 + n2
+// }
+// console.log(sum(1,1))
